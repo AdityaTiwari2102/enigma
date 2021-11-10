9f4835d668025ec138874cf2915ab2b80752316d
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-11-10 19:05:14 +0530

    Feat: Toggle sidenav element

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,12 @@ import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
